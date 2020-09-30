1c7b2a6bdf13b6c4515d31be0f411628dba54c6f
Simon Huang <simh1989@gmail.com>
2020-09-30 21:26:03 +0000

    kick off

---
+++ check_etl_status.py
@@ -0,0 +1,52 @@
+import psycopg2
+from psycopg2 import sql
+
+from sqlalchemy import create_engine
+
+import pandas as pd
+
+import requests
+import json
+
+from discord_webhook import DiscordWebhook
+
+
+####### Parameters #######
+POSTGRES_ADDRESS = 'localhost'
+POSTGRES_PORT = 5432
+POSTGRES_USERNAME = 'etl'
+POSTGRES_PASSWORD = 'etl'
+POSTGRES_DBNAME =  'etl'
+
+DISCORD_URL = "https://discord.com/api/webhooks/755075772267626558/jeHPq3kFHj5oAVisPBvZdlez_7p8arBUmMwmwQec3RWbShKJ2Q7PEkR098kihZQ1yc8b"
+
+try:
+    # Connect to local database
+    postgres_str = ('postgresql://{username}:{password}@{ipaddress}:{port}/{dbname}'.format(username=POSTGRES_USERNAME, 
+                                                                                        password=POSTGRES_PASSWORD, 
+                                                                                        ipaddress=POSTGRES_ADDRESS, 
+                                                                                        port=POSTGRES_PORT, 
+                                                                                        dbname=POSTGRES_DBNAME))
+
+    # connecting to a PostgreSQL database
+    cnx = create_engine(postgres_str)
+
+    count_blocks = pd.read_sql_query("SELECT count(*) FROM blocks", cnx)
+    #count_gateways = pd.read_sql_query("SELECT COUNT(*) FROM gateway_inventory;", cnx)
+
+    etl_height = count_blocks.iloc[0]['count']
+
+    cnx.dispose()
+except Exception as e:
+    print(e)
+    etl_height = "N/A"
+
+helium_explorer_height = requests.get("https://api.helium.io/v1/blocks/height")
+if helium_explorer_height.status_code == 200:
+    helium_height = helium_explorer_height.json()["data"]["height"]
+else:
+    helium_height = "N/A"
+
+webhook = DiscordWebhook(url=DISCORD_URL, content=f'Helium Blocks: {helium_height} | Emrit ETL Blocks: {etl_height}')
+response = webhook.execute()
+
---
+++ run_etl_check.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source /home/simh1989/check_etl_status/venv/bin/activate
+python /home/simh1989/check_etl_status/check_etl_status.py 
